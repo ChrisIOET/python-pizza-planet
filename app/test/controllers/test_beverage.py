bc71884aa06329e576df90383e271cf664c7c9a7
ChrisIOET <christian.scarpati@ioet.com>
2022-08-15 17:27:04 -0300

    test beverage changed

--- app/test/controllers/test_beverage.py
+++ app/test/controllers/test_beverage.py
@@ -24,7 +24,6 @@ def test_update(app, beverage: dict):
     })
     pytest.assume(error is None)
     beverage_from_database, error = BeverageController.get_by_id(created_beverage['_id'])
-    pytest.assume(error is None)
     for param, value in updated_fields.items():
         pytest.assume(updated_beverage[param] == value)
         pytest.assume(beverage_from_database[param] == value)
@@ -39,16 +38,12 @@ def test_get_by_id(app, beverage: dict):
 
 
 def test_get_all(app, beverages: list):
-    created_beverages = []
-    for beverage in beverages:
-        created_beverage, _ = BeverageController.create(beverage)
-        created_beverages.append(created_beverage)
-
     beverages_from_db, error = BeverageController.get_all()
     searchable_beverages = {db_beverage['_id']: db_beverage for db_beverage in beverages_from_db}
     pytest.assume(error is None)
-    for created_beverage in created_beverages:
-        current_id = created_beverage['_id']
-        assert current_id in searchable_beverages
-        for param, value in created_beverage.items():
-            pytest.assume(searchable_beverages[current_id][param] == value)
+    for beverage in beverages:
+        pytest.assume(beverage['_id'] in searchable_beverages)
+        for param, value in beverage.items():
+            pytest.assume(searchable_beverages[beverage['_id']][param] == value)
+
+    
