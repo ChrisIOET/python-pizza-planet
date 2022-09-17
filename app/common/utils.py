79db851c357a9bd65847ad347e2d0765f3782b34
Christian Scarpati <christian-mallmann@hotmail.com>
2022-09-17 13:42:51 -0300

    refactor(seed): code improved

--- app/common/utils.py
+++ app/common/utils.py
@@ -1,6 +1,2 @@
 def check_required_keys(keys: tuple, element: dict):
     return all(element.get(key) for key in keys)
-
-
-def format_currency_with_double_decimals(element: dict) -> float:
-    return float("{:.2f}".format(round(element, 2)))
